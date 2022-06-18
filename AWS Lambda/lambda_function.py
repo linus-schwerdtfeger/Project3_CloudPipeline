a9ddf7b28afccb640b31d7bcfec5fde9541fec12
linus-schwerdtfeger <linus.profiles@mail.de>
2022-06-18 21:53:09 +0200

    Improved structure

--- AWS Lambda/lambda_function.py
+++ AWS Lambda/lambda_function.py
@@ -4,10 +4,12 @@ from datetime import datetime
 import pandas as pd
 import sqlalchemy
 import flights
+import weather
 
 def lambda_handler(event, context):
       
     flights.get_flights()
+    weather.get_weather()
 
     return {
         'statusCode': 200,
